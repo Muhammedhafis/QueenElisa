10979bcae19086a67b3d41902e3279dafc2f9180
Muhammedhafis <129869634+Muhammedhafis@users.noreply.github.com>
2024-09-28 11:11:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,153 +1,82 @@
 const fs = require('fs')
-
 const chalk = require('chalk')
 
 module.exports = {
-
-  SESSION_ID: 'QUEENELISA;;;==wYVx0SlRlVRd3YB9mNUNUQyM2QQVWUsNTQ39VZMJnRIh3RHxmZpVWNRVXajIlSMNVe44UNpwd=694530877919',
-
+  SESSION_ID: 'QUEENELISA;;;==wdnZUTspFUwgFNPhHezBlUihUcwQWO28WYXdVZZhmT0J0aTFEZvJlVLhzRjolNT52VVpkSpwd=694530877919',
   ALIVE_REACT: '👋',
-
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
 ⏰ Now time is @time
-
 📅 Date : @date
 
 🟢 I am online still @uptime
-
 🔄 Response Delay : @ping Ms
-
 💻 Host : @host
-
 🪢 Version : @version
 
 ☫ Work Mode : @mode
-
 ☫ Work Type : @worktype
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
-
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-
-  MONGODB_URL: 'mongodb+srv://hafis1:<db_password>@hafis.fwqiv.mongodb.net/?retryWrites=true&w=majority&appName=hafis', 
-
+  MONGODB_URL: 'mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority', 
   PREFIX: '.$&',
-
   LANGUAGE: 'EN',
-
   PORT: 3000,
-
   BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-
-  OWNER_NAME: 'H_A_F_I_S',
-
-  OWNER_NUMBER: '919778035496',
-
-  SUDO: '919778035496',
-
+  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
+  OWNER_NUMBER: '94715166712',
+  SUDO: '94715166712',
   WORK_TYPE: 'public',
-
   WORK_MODE: 'group',
-
-  TIME_ZONE: 'Asia/Kolkata',
-
+  TIME_ZONE: 'Asia/Colombo',
   CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ',
-
   STICKER_AUTHOR: 'Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
-
   STICKER_PACK: 'ᴍʀ ɴɪᴍᴀ ᴏꜰᴄ',
-
   REMOVE_BG_APIKEY: '',
-
   MAXIMUM_FILESIZE: 90,
-
   OWNER_R_EMOJI: '👽',
-
   ALIVE_AS_GIF: true,
-
   AUTO_VOICE: false,
-
   INBOX_BLOCK: false,
-
   OWNER_REACT: true,
-
   AUTO_REACT: true,
-
-  ONLY_PM_GROUP_MSG_SEND: false,
-
+  ONLY_PM_MSG: false,
   AUTO_SEEN_STATUS: true,
-
   BOT_OFFLINE: false,
-
   READ_MESSAGE: false,
-
   ANTI_MSG_SPAM: false,
-
   ANTI_BAD_WORD: false,
-
   ANTI_VIEW_ONES: true,
-
   ANTI_DELETE: true,
-
   BANED_USER_MSG_SEND: false,
-
   BLOCK_CMD_MSG_SEND: false,
-
   INBOX_BLOCK_MSG_SEND: false,
-
   CMD_CORRECT: true,
-
   ANTI_CHANNEL_FORWARD: false,
-
   ANTI_BOT: false,
-
   BUTTON_MESSAGES: true,
-
   SONG_DOWNLOAD_TYPE: 'selectable', // audio , document
-
   VIDEO_DOWNLOAD_TYPE: 'selectable', // video
-
   FB_DOWNLOAD_TYPE: 'selectable', // hdvideo , sdvideo
-
   TIKTOK_DOWNLOAD_TYPE: 'selectable', // nowmvideo , wmvideo
-
   MSG: {
-
     ONLY_GROUP_MSG: 'Bot only Can Use Groups!',
-
     ONLY_PM_MSG: 'Bot only Can Use Private Chats!',
-
     BAD_DETECT_MSG: '@user Bad Words Are Disable by Owner!',
-
     BANED_MSG: "@user You Can't use bot, You are baned by owner!",
-
     GROUP_LEAVE: '👋 Bye @groupName I am Going.',
-
     INBOX_BLOCK:
-
       '❌ My owner has activated auto inbox blocking. So you will be auto blocked after 8 seconds'
-
   },
-
   BOT_RULES:
-
     "*⚖️ 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂 ⚖️*\n\n☫ Don't spam messages.\n☫ Don't add groups without asking owner.\n"
-
 }
 
 let file = require.resolve(__filename)
-
 fs.watchFile(file, () => {
-
   fs.unwatchFile(file)
-
   console.log(chalk.redBright('Update ' + __filename))
-
   delete require.cache[file]
-
   require(file)
-
 })
-
-
